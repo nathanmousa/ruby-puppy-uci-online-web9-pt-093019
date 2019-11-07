013a2488d866151b3d3f0abe3c62bff747ed5032
Nathan Mousa <nathanmousa1@gmail.com>
2019-11-07 02:30:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,5 @@
 require 'pry'
 class Dog
-<<<<<<< HEAD
   attr_accessor :name
   @@all = []
   
@@ -11,16 +10,6 @@ class Dog
   
   def save
     @@all << self
-=======
-  
-  @@all = []
-  attr_accessor :name
-  
-  def initialize(name)
-    @name = name
-    
-    @@all << self.name
->>>>>>> cf433f77157ecfbdafccd185c232d1d0321f5839
   end
   
   def self.all
@@ -32,12 +21,6 @@ class Dog
   end
   
   def self.print_all
-<<<<<<< HEAD
     @@all.each { |dog| puts "#{dog.name}" }
   end
-=======
-    @@all.each { |name| puts "#{name}" }
-  end
-  binding pry
->>>>>>> cf433f77157ecfbdafccd185c232d1d0321f5839
 end
